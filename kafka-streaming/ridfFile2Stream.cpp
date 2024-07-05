c23eee88aff3405b0f531073cf3ee3c55e8a5926
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2024-07-05 18:39:30 +0900

    kafa-streaming added

---
+++ kafka-streaming/ridfFile2Stream.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <librdkafka/rdkafka.h>
+#include "TRIDFEventStore.h"
+#include "mira_streaming.hpp"
+
+// word size for RIDF (=4)
+static const u_int32_t kWordSize = 4;
+
+// Global variables for Kafka (assuming they are defined somewhere else)
+bool kafka_ready = false; // Placeholder
+
+void readRIDF(std::string ridf_file_name, rd_kafka_topic_t *topic, u_int64_t n_block)
+{
+    // Open ridf file
+    std::ifstream ridf(ridf_file_name, std::ios::binary);
+    if (!ridf)
+    {
+        std::cout << "Cannot open file: " << ridf_file_name << std::endl;
+        return;
+    }
+    std::cout << "File opened: " << ridf_file_name << std::endl;
+
+    // Get total size
+    ridf.seekg(0, std::ios::end);
+    u_int64_t size = ridf.tellg();
+    std::cout << "File size: " << size << " bytes." << std::endl;
+    ridf.seekg(0);
+
+    u_int64_t block_count = 0;
+    while (true)
+    {
+        // Break if end of file
+        if (ridf.eof())
+            break;
+
+        u_int32_t block_size;
+        u_int32_t class_id;
+        u_int32_t layer;
+
+        auto readHeader = [&ridf](u_int32_t &block_size, u_int32_t &class_id, u_int32_t &layer)
+        {
+            char header[kWordSize];
+            ridf.read(header, kWordSize);
+            u_int32_t *header32 = (u_int32_t *)header;
+            block_size = (header32[0] & 0x003fffff);
+            class_id = ((header32[0] & 0x0fc00000) >> 22);
+            layer = ((header32[0] & 0x30000000) >> 28);
+            ridf.seekg(ridf.tellg() - kWordSize);
+            // std::cout << "read block: " << block_size << ", class id: " << class_id << ", layer: " << layer << std::endl;
+        };
+
+        // Read header and get the block size
+        readHeader(block_size, class_id, layer);
+
+        // Break if the block size is greater than EoF
+        if (ridf.tellg() + block_size * 2 > size)
+            break;
+
+        // Read the block
+        auto buff = std::make_unique<char[]>(block_size * 2);
+        ridf.read(buff.get(), block_size * 2);
+
+        // Placeholder for Kafka producer logic
+        mira::produce(topic, block_size, buff.get());
+        ++block_count;
+
+        if (!(block_count % 1000))
+        {
+            double progress = (double)ridf.tellg() / (double)size * 100.;
+            std::cout << "block_count: " << block_count << ", " << ridf.tellg() << "bytes/" << size << "bytes (" << progress << "\%)" << std::endl;
+        }
+
+        //  Break if the block size is greater than EoF
+        if (block_count > n_block)
+            break;
+    }
+    ridf.close();
+    std::cout
+        << "Terminating" << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        std::cout << "Usage: ridfFile2Stream [input_file] [bootstrap_servers] [n_block(optional)]" << std::endl;
+        return 1;
+    }
+    std::string input_file_name = argv[1];  // input file name
+    std::string bootstrap_servers(argv[2]); // Kafka bootstrap.servers
+    u_int64_t n_block = -1;
+    if (argc == 4)
+        n_block = std::atoi(argv[3]);
+    // initialize a Kafka producer
+    auto topic = mira::init_producer("0", bootstrap_servers, "ridf-test");
+    // main loop
+    readRIDF(input_file_name, topic, n_block);
+    return 0;
+}
---
+++ kafka-streaming/stream_artemis.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <TFolder.h>
+#include <TROOT.h>
+#include "TLoopManager.h"
+#include "TModuleDecoderFactory.h"
+#include "TModuleDecoderFixed.h"
+#include "TModuleDecoderV7XX.h"
+#include "TModuleDecoderV767.h"
+#include "TModuleDecoderV1190.h"
+#include "TModuleDecoderV1290.h"
+#include "TModuleDecoderSIS3820.h"
+#include "TModuleDecoderSIS3610.h"
+#include "TModuleDecoderMXDC32.h"
+#include "TModuleDecoderSkip.h"
+#include "TModuleDecoderA3100FreeRunTSI.h"
+#include "TModuleDecoderV1740_mod.h"
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        std::cout << "Usage: stream_artemis [steering_file] [bootstrap.servers] [client.id (= 0)]" << std::endl;
+        return 1;
+    }
+    std::string filename = argv[1];
+    std::string bootstrap_servers = argv[2];
+    std::string client_id = "0";
+    if (argc > 3)
+    {
+        client_id = argv[3];
+    }
+
+    art::TLoopManager *man = art::TLoopManager::Instance();
+    std::map<std::string, std::string> replace;
+
+    art::TModuleDecoderFactory *df = art::TModuleDecoderFactory::Instance();
+    // mod ID 0 : Fixed16
+    const Int_t digits0 = 16;
+    df->Register(new art::TModuleDecoderFixed<unsigned short>(0, digits0));
+    // mod ID 1 : Fixed24
+    const Int_t digits1 = 24;
+    df->Register(new art::TModuleDecoderFixed<unsigned int>(1, digits1));
+    // mod ID 21 : V7XX
+    df->Register(new art::TModuleDecoderV7XX);
+    // mod ID 23 : V767
+    df->Register(new art::TModuleDecoderV767);
+    // mod ID 24 : V1190
+    // mod ID 25 : V1290
+    // mod ID 26 : V1190C
+    art::TModuleDecoder *v1190 = new art::TModuleDecoderV1190;
+    art::TModuleDecoder *v1290 = new art::TModuleDecoderV1290(25);
+    art::TModuleDecoder *v1190c = new art::TModuleDecoderV1190(26);
+    v1190->SetVerboseLevel(kError);
+    v1290->SetVerboseLevel(kError);
+    v1190c->SetVerboseLevel(kError);
+    df->Register(v1190);
+    df->Register(v1290);
+    df->Register(v1190c);
+    // mod ID 36 : SIS3820
+    df->Register(new art::TModuleDecoderSIS3820);
+    df->Register(new art::TModuleDecoderSIS3610);
+    df->Register(new art::TModuleDecoderMXDC32);
+
+    df->Register(new art::TModuleDecoderSkip(42));
+
+    df->Register(new art::TModuleDecoderSkip(8));
+    // mod ID 59 : AMTTDC
+    //    gInterpreter->ProcessLine("art::TModuleDecoderFactory::Instance()->Register(new art::TModuleDecoderAMTTDC);");
+    //    df->Register(new art::TModuleDecoderAMTTDC);
+
+    df->Register(new art::TModuleDecoderA3100FreeRunTSI);
+    df->Register(new art::TModuleDecoderV1740_mod);
+
+    // Skip for r3_2021
+    df->Register(new art::TModuleDecoderSkip(9));
+    df->Register(new art::TModuleDecoderSkip(30));
+    df->Register(new art::TModuleDecoderSkip(33));
+    df->Register(new art::TModuleDecoderSkip(30));
+
+    // Preparation of folder for artemis
+    TFolder *top = gROOT->GetRootFolder()->AddFolder("artemis", "artemis top level folders");
+    gROOT->GetListOfBrowsables()->Add(top);
+
+    man->Add(filename.c_str(), &replace);
+    man->Resume();
+    return 0;
+}
